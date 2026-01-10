401fa582b8fbe90cd9b1978f4c450ae6ec21f2d1
phuc.nguyen <hphuc.np@gmail.com>
2026-01-10 21:22:53 +0700

    wip

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,6 @@ from blog.models import Tag, Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    exclude = ("modified_at", "slug")
 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
